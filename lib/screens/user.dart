653a691aa406dbf8b368c9dec7e2f9d69d4bd8b1
Catbox <catbox082@hotmail.com>
2021-06-30 23:59:06 +0700

    fourth

--- lib/screens/user.dart
+++ lib/screens/user.dart
@@ -16,7 +16,54 @@ class _UserScreenState extends State<UserScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        //       appBar: buildAppBar(context),
+//       body: ListView(
+//         physics: BouncingScrollPhysics(),
+//         children: [
+//           ProfileWidget(
+//             imagePath: user.imagePath,
+//             onClicked: () async {
+
+//             },
+//           ),
+//           const SizedBox(height: 24),
+//           buildName(user),
+//           const SizedBox(height: 48),
+//           buildAbout(user),
+//         ],
+//       ),
+//     );
+//   }
+
+//   Widget buildName(User user) => Column(
+//         children: [
+//           Text(user.name,
+//               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24)),
+//           const SizedBox(height: 4),
+//           Text(
+//             user.email,
+//             style: TextStyle(color: Colors.grey),
+//           ),
+//         ],
+//       );
+//   Widget buildAbout(User user) => Container(
+//         padding: EdgeInsets.symmetric(horizontal: 48),
+//         child: Column(
+//           crossAxisAlignment: CrossAxisAlignment.start,
+//           children: [
+//             Text(
+//               'About',
+//               style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+//             ),
+//             const SizedBox(height: 16),
+//             Text(
+//               user.about,
+//               style: TextStyle(fontSize: 16, height: 1.4),
+//             ),
+//           ],
+//         ),
+        );
   }
 }
 //       appBar: buildAppBar(context),
